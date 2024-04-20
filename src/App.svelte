c19875dfe5e203f7ad6573c6d72bcd94d6075d51
Marc Marquez <marc.marquez@gmail.com>
2024-04-20 13:02:05 -0700

    Create card component and add brand section

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,23 @@
 <script>
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
+	import Card from "./components/Card.svelte";
+
+    let brands = ['Nike', 'Adidas', 'New Balance', 'Converse', 'Reebok'];
+
 </script>
 
 <Header name="Sneaks"/>
 <main>
-
+	<section class="flex-row flex-wrap">
+        {#each brands as brand}
+        <Card>
+            <div class="flex-row">
+                <img class="brand-image" src="{`https://logo.clearbit.com/${brand.replace(/\s+/g, '')}.com`}?size=75" />
+            </div>
+        </Card>
+        {/each}
+    </section>
 </main>
 <Footer />
 
@@ -15,6 +27,16 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+
+	.flex-wrap {
+		flex-wrap: wrap;
+	}
+
+	.flex-row {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
 	}
 
 	@media (min-width: 640px) {
---
+++ src/components/Card.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let bgcolor = 'black';
+    export let color = 'white';
+    export let margin = '10px';
+    export let border = '1px solid black';
+    export let width = '200px';
+    export let height = '150px';
+</script>
+
+<div 
+    class="card" 
+    style="background-color: {bgcolor}; color: {color}; margin: {margin}; width: {width}; height: {height}"
+>
+    <slot></slot>
+</div>
+
+<style>
+    .card {
+        width: 200px;
+        height: 150px;
+        padding: .5px;
+        border-radius: 1rem;
+        border: 5px solid transparent;
+        background-color: black;
+        margin: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    .card:hover {
+        border: 5px solid red;
+        cursor: pointer;
+    }
+</style>

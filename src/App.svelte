e10fe7475c43318836cf64900d0f3bea2e52b39d
Marc Marquez <marc.marquez@gmail.com>
2024-04-20 13:12:39 -0700

    Get shoes and show in grid

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,28 @@
 	import Card from "./components/Card.svelte";
 
     let brands = ['Nike', 'Adidas', 'New Balance', 'Converse', 'Reebok'];
+    let shoes = [];
+    let totalPages = 0;
+    let currentPage = 1;
+    let currentBrand = '';
+    let details;
+
+
+	const getShoes = (brand, page=1) => {
+        fetch(`https://api.stockx.vlour.me/search?query=${brand} shoes&page=${page}`)
+            .then(response => response.json())
+            .then(data => {
+                shoes = data.hits;
+                totalPages = data.pages;
+				console.log(shoes);
+            });
+    }
+
+	const setBrandAndGet = (brand) => {
+        currentBrand = brand;
+        currentPage = 1;
+        getShoes(currentBrand);
+    };
 
 </script>
 
@@ -12,9 +34,17 @@
 	<section class="flex-row flex-wrap">
         {#each brands as brand}
         <Card>
-            <div class="flex-row">
-                <img class="brand-image" src="{`https://logo.clearbit.com/${brand.replace(/\s+/g, '')}.com`}?size=75" />
+            <div class="flex-col">
+                <img class="brand-image" alt="{brand} logo"src="{`https://logo.clearbit.com/${brand.replace(/\s+/g, '')}.com`}?size=75" />
+                <button style="margin: 5px;" on:click={() => setBrandAndGet(brand)}>Get Shoes</button>
             </div>
+        </Card>
+        {/each}
+    </section>
+	<section class="flex-row flex-wrap">
+        {#each shoes as shoe}
+        <Card bgcolor="white" border="none">
+            <img class="shoe-image" src={shoe.image} alt={shoe.name}/>
         </Card>
         {/each}
     </section>
@@ -37,6 +67,17 @@
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
+	}
+
+	.flex-col {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	.shoe-image {
+		object-fit: contain;
+		width: 80%;
 	}
 
 	@media (min-width: 640px) {
